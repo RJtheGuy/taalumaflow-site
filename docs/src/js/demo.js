53c9dace3478ca95907bd2dd7fd028e7482a8562
Rasheed Juma <rashidj.mwinyi@gmail.com>
2026-08-19 06:34:20 -0700

    Update demo.js

--- docs/src/js/demo.js
+++ docs/src/js/demo.js
@@ -96,7 +96,6 @@ export function initExtractionDemo() {
           capturedEmail = 'skipped';
           runExtraction(textarea, runBtn, resultEl, emptyEl, loadingEl, '');
         }
-        }
       );
     } else {
       runExtraction(textarea, runBtn, resultEl, emptyEl, loadingEl, capturedEmail === 'skipped' ? '' : capturedEmail);
